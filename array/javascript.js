cd169c884a735eebe355294a687464055eccfaf2
teunvanderploeg <54798753+teunvanderploeg@users.noreply.github.com>
2019-11-29 12:24:19 +0100

    array
    array map er bij

---
+++ array/javascript.js
@@ -0,0 +1,29 @@
+var resept = document.getElementById("eten");
+
+let eten = [
+  " Spaghetti met ham en kaas ",
+  " Macaroni met ham en kaas ",
+  " Tagliatelli met ham en kaas ",
+  " Spaghetti met spinazie en room ",
+  " Macaroni met spinazie en room ",
+  " Tagliatelli met spinazie en room ",
+  " Spaghetti met gehakt-tomatensaus en kaas ",
+  " Macaroni met gehakt-tomatensaus en kaas ",
+  " Tagliatelli met gehakt-tomatensaus en kaas ",
+  " Spaghetti met spekjes, spinazie en room ",
+  " Macaroni met spekjes, spinazie en room ",
+  " Tagliatelli met spekjes, spinazie en room ",
+  " Spaghetti met courgette en tomatensaus ",
+  " Macaroni met courgette en tomatensaus ",
+  " Tagliatelli met courgette en tomatensaus ",
+  " Lasagne met courgette en tomatensaus ",
+  " Lasagne met room, doperwten en tomatensaus ",
+  " Lasagne met spinazie en tomatensaus en kaas "
+];
+
+food();
+function food() {
+  let r = Math.floor(Math.random() * 19);
+
+  resept.innerHTML = eten[r];
+}
