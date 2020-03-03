45d3f7a5c21602193119d8dd016d591a1aa401de
Teun van der Ploeg <29253@ma-web.nl>
2020-03-03 19:31:44 +0100

    animatie
    ik heb een animatie gemaakt en en terug button

--- ModaalVenster/js/lightbox.js
+++ ModaalVenster/js/lightbox.js
@@ -8,8 +8,40 @@ for (let i = 0; i < alleInfo.length; i++) {
   alleInfo[i].remove();
 }
 
+function verwijderModaal() {
+  document.getElementById("modaal").remove();
+}
+
 function maakModaal(num) {
   console.log(alleInhoud[num]);
+  let modaal = document.createElement("div");
+  modaal.className = "modaal";
+  modaal.id = "modaal";
+  modaal.addEventListener("click", verwijderModaal);
+
+  //sluitknop
+  let sluitknop = document.createElement("i");
+  sluitknop.className = "fas fa-skull-crossbones sluit-knop"; //doodshooft sluitknop
+  sluitknop.addEventListener("click", verwijderModaal);
+
+  let modaalInhoud = document.createElement("div");
+  modaalInhoud.className = "modaalInhoud";
+  modaalInhoud.innerHTML = alleInhoud[num].innerHTML;
+  modaalInhoud.addEventListener("click", function(e) {
+    e.stopPropagation();
+  });
+
+  // gsap.to(modaalInhoud, {
+  //   maringTop: 0,
+  //   duration: 1,
+  //   ease: "elastic.out(1, 0,3)"
+  // });
+
+  modaalInhoud.prepend(sluitknop);
+
+  modaal.append(modaalInhoud);
+
+  document.body.append(modaal);
 }
 
 for (let i = 0; i < alleBlijvend.length; i++) {
