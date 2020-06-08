4339b5fb3e3c03f074aebbae1d26ea6bf58df503
Teun van der Ploeg <29253@ma-web.nl>
2020-06-08 17:27:09 +0200

    week 2
    week 2

--- themes/TeunThema/index.php
+++ themes/TeunThema/index.php
@@ -1,3 +1,12 @@
-<?php ?>
-<h2>Welkom<h2>
-<p>Bij Teun zijn thema<p>
+<?php
+if( have_post()):
+    while(have_post()) : the_post();?>
+        <h3><?php the_title() ?></h3>
+        <div><?php the_content() ?></div>
+<?php 
+    endwhile;
+else: ?>
+    <p>geen berichten gevonden</p>
+<?php 
+endif; 
+?>
