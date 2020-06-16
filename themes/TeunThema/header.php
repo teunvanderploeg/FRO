f63ddbba6837c9dd8a3d11b03926c69fefd8a9d0
Teun van der Ploeg <29253@ma-web.nl>
2020-06-16 13:22:20 +0200

    css probleemen opgelost
    css probleemen opgelost door header en footer er goed in te zetten

--- themes/TeunThema/header.php
+++ themes/TeunThema/header.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="<?php bloginfo('authos')?>">
     <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
 </head>
 <body>
 
-<h1><?php bloginfo('name'); ?></h1>
+<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
