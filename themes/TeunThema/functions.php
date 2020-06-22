1ea13837acd99450c4758c0637ad1e98accfff05
Teun van der Ploeg <29253@ma-web.nl>
2020-06-22 16:38:53 +0200

    menu toegevoegt
    menu toegevoegt met bootstrap en js

--- themes/TeunThema/functions.php
+++ themes/TeunThema/functions.php
@@ -1,12 +1,13 @@
 <?php 
 
-// load styleblad
-function laadStijlblad(){
+// load styleblad en scripts
+function laadStijlbladenScripts(){
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
+    wp_enqueue_style( 'bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array ( 'jquery' ), 1.5, true);
 }
 
-add_action( 'init', 'laadStijlblad' );
+add_action( 'init', 'laadStijlbladenScripts' );
 
 
 function register_menus() {
@@ -20,10 +21,12 @@ function register_menus() {
   add_action( 'init', 'register_menus' );
 
 
+  function register_navwalker(){
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+  }
+  add_action( 'after_setup_theme', 'register_navwalker' );
 
 
 
 
 
-
-?>
--- themes/TeunThema/header.php
+++ themes/TeunThema/header.php
@@ -9,13 +9,27 @@
 </head>
 <body>
 <header>
-    <nav>
-    <?php wp_nav_menu( 
-        array(
-            'theme_location' => 'hoofd-menu'
-            ) 
-        ); ?>
-    </nav>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Teun Site</a>
+        <?php
+        wp_nav_menu( array(
+            'theme_location'    => 'hoofd-menu',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'bs-example-navbar-collapse-1',
+            'menu_class'        => 'nav navbar-nav',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker(),
+        ) );
+        ?>
+    </div>
+</nav>
 <div class="jumbotron">
     <h1 class="display-4"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
     <p class="lead"><?php bloginfo('description'); ?></p>
