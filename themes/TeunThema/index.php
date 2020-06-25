1c09626d9b0b3238deff843b1b206ee61455bbd4
Teun van der Ploeg <29253@ma-web.nl>
2020-06-25 20:10:01 +0200

    layout
    layout beter gemaakt

--- themes/TeunThema/index.php
+++ themes/TeunThema/index.php
@@ -1,22 +1,29 @@
 <?php get_header(); ?>
 
 
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <main class="col-md-8">
-            <?php 
-            if ( have_posts() ) : 
-                while ( have_posts() ) : the_post();  ?>
-                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                    <div><?php the_excerpt(); ?></div>
-                <?php endwhile; 
-            else: ?> 
-                <p>Geen bereichten gevonden.</p>
-            <?php endif; ?>
+        <main class="col-md-9 container">
+            <div class="row">
+                <?php 
+                if ( have_posts() ) : 
+                    while ( have_posts() ) : the_post();  ?>
+                    <div class="col-md-6">
+                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <div><?php the_excerpt(); ?></div>
+                            <div class="text-center p-4">
+                                <a href="<?php the_permalink(); ?>" class="btn-outline-primary">Lees meer..</a>
+                            </div>
+                    </div>
+                    <?php endwhile; 
+                else: ?> 
+                    <p>Geen bereichten gevonden.</p>
+                <?php endif; ?>
+            </div>
         </main>
         
         
-        <aside class="col-md-4 bg-light p-4">
+        <aside class="col-md-3 bg-light p-4">
             <?php dynamic_sidebar('aside') ?>
         </aside>
     </div>
